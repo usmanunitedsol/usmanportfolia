2a51af0b9d401112b2b5a92ff2e8019caff0a7b1
usman_sol <usman.liaqut@unitedsol.net>
2023-05-26 14:51:50 +0500

    project page design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-        Project
+    <div id='projects' style={{  paddingTop: "50px" }} className=' project_section container'>
+
+        <div className='row'>
+        <h2 className="text-center"  style={{ fontSize: "2.1em"}}>
+                MY Recent <strong className="purple">Works</strong>
+            </h2>
+            <p className="text-center" style={{ paddingBottom: "30px" }} >Here are a few projects I've worked on recently.</p>
+          <div className='col'></div>
+        </div>
     </div>
   )
 }
